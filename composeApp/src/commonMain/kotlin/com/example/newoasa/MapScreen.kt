93bd864c37c3c67fbb3f281df7eacdbc5e550a34
Filippos Kranakis <kranakis@ianic.gr>
2026-02-05 10:56:30 +0200

    Fix MapScreen.kt: Add missing import for dp

--- composeApp/src/commonMain/kotlin/com/example/newoasa/MapScreen.kt
+++ composeApp/src/commonMain/kotlin/com/example/newoasa/MapScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
 @Composable
