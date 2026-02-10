737cadaf9a4cf481f0c80322f63647291464b26e
Filippos Kranakis <kranakis@ianic.gr>
2026-02-10 13:29:10 +0200

    Remove duplicate PreferenceStorageProvider.kt

--- composeApp/src/commonMain/kotlin/com/example/newoasa/utils/PreferenceStorageProvider.kt
+++
@@ -1,8 +0,0 @@
-package com.example.newoasa.utils
-
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import com.example.newoasa.data.PreferenceStorage
-
-@Composable
-expect fun rememberPreferenceStorage(): PreferenceStorage
