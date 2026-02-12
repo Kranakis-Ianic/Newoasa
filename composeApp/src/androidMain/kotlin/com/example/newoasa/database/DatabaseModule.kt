89e54d53273a861f7678fbab395ae09e6b8617ce
Filippos Kranakis <kranakis@ianic.gr>
2026-02-12 12:16:53 +0200

    Commit works

--- composeApp/src/androidMain/kotlin/com/example/newoasa/database/DatabaseModule.kt
+++ composeApp/src/androidMain/kotlin/com/example/newoasa/database/DatabaseModule.kt
@@ -1,14 +1,17 @@
 package com.example.newoasa.database
 
+import com.example.newoasa.database.room.TransitDatabase
+import com.example.newoasa.database.room.getDatabase
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 /**
  * Koin module for Android-specific database dependencies.
+ * Updated to use the KMP Room Database.
  */
 val databaseModule = module {
-    // Provide the TransitDatabase instance using the existing getInstance method
-    single { TransitDatabase.getInstance(androidContext()) }
+    // Provide the KMP TransitDatabase using the KMP builder function
+    single<TransitDatabase> { getDatabase(androidContext()) }
 
     // Provide the DAOs via the database instance
     single { get<TransitDatabase>().transitLineDao() }
