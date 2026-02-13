de97c086db2335b6f5b48499fd8318ad50dfd9c4
Filippos Kranakis <kranakis@ianic.gr>
2026-02-13 16:08:57 +0200

    Commit not works

--- composeApp/src/commonMain/kotlin/com/example/newoasa/utils/MapView.kt
+++ composeApp/src/commonMain/kotlin/com/example/newoasa/utils/MapView.kt
@@ -4,10 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.example.newoasa.data.TransitLine
-import org.maplibre.compose.MaplibreMap
-import org.maplibre.compose.ramani.MapLibre
-import org.maplibre.compose.rememberSaveableMapViewCameraState
-import org.maplibre.geojson.Point
+import org.maplibre.compose.camera.rememberCameraState
+import org.maplibre.compose.map.MaplibreMap
+import org.maplibre.compose.style.BaseStyle
+import org.maplibre.compose.style.rememberStyleState
 
 /**
  * Unified MapView for both Android and iOS platforms
